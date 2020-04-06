7bbd2e822795433f2a64c7a6bf657ca94e8360a8
Rimpei Kunimoto <kunimotorimpei@gmail.com>
2020-04-06 03:38:31 +0000

    コピペ部分を自分で書き直した

--- lib/riff.hpp
+++ lib/riff.hpp
@@ -1,52 +1,34 @@
 #include <fstream>
 #include <iostream>
 
-// http://chichimotsu.hateblo.jp/entry/20110812/1313173801
-
 class RIFF
 {
   private:
     std::ofstream of;
-    int riff_p = 0;
-    int data_p = 0;
+    int riffStartPos = 0;
+    int dataStartPos = 0;
 
   public:
     // ヘッダを作る
     RIFF(const char *fname, unsigned int channelCount, unsigned int bitSize, unsigned long int samplingRate)
     {
-        int buf = 0;
-        short buf2 = 0;
-        of.open(fname, std::ios::out | std::ios::binary | std::ios::trunc);
-        of.seekp(0, std::ios::beg); // ファイルポインタを頭へ
-
-        of.write("RIFF", 4); // RIFFヘッダ
-        riff_p = of.tellp();
-        of.seekp(sizeof(char) * 4, std::ios::cur); // サイズを出力するための場所を空ける
-        of.write("WAVE", 4);                       // WAVEヘッダ
-        of.write("fmt ", 4);                       // fmt ヘッダ
-        buf = 16;
-        of.write((char *)&buf, 4); // fmt ヘッダ長
-        buf2 = 1;
-        of.write((char *)&buf2, 2); // リニアPCM
-
-        buf2 = channelCount;
-        of.write((char *)&buf2, 2); // チャネル数
-
-        buf = samplingRate;
-        of.write((char *)&buf, 4); // サンプリングレート
-
-        buf = samplingRate * channelCount;
-        of.write((char *)&buf, 4); // データ速度 チャンネル数 * サンプリングレート
-
-        buf2 = (bitSize / 2) * channelCount;
-        of.write((char *)&buf2, 2); // ブロックサイズ 16bit (2byte) * チャンネル数
-
-        buf2 = bitSize;
-        of.write((char *)&buf2, 2); // サンプルあたりのビット数
-
-        of.write("data", 4); // dataヘッダ
-        data_p = of.tellp();
-        of.seekp(sizeof(char) * 4, std::ios::cur); // 4バイト分進める
+        of.open(fname, std::ios::out | std::ios::binary | std::ios::trunc);                 // ファイルを開く
+        of.seekp(0, std::ios::beg);                                                         // ファイルポインタをファイル先頭に
+        of.write("RIFF", 4);                                                                // RIFFヘッダ
+        riffStartPos = of.tellp();                                                          // RIFFヘッダ終了位置を保存しておく
+        of.seekp(sizeof(char) * 4, std::ios::cur);                                          // 終了までわからないチャンクサイズ書き込み用スペースを確保
+        of.write("WAVE", 4);                                                                // WAVEヘッダ
+        of.write("fmt ", 4);                                                                // fmt ヘッダ
+        of.write(reinterpret_cast<const char *>(new int(16)), 4);                           // fmt ヘッダ長
+        of.write(reinterpret_cast<const char *>(new int(1)), 2);                            // LPCM
+        of.write(reinterpret_cast<const char *>(&channelCount), 2);                         // チャネル数
+        of.write(reinterpret_cast<const char *>(&samplingRate), 4);                         // サンプリングレート
+        of.write(reinterpret_cast<const char *>(new int(samplingRate * channelCount)), 4);  // データレート チャンネル数 * サンプリングレート
+        of.write(reinterpret_cast<const char *>(new int((bitSize / 8) * channelCount)), 2); // ブロックサイズ 量子化ビット数 / 8 * チャンネル数
+        of.write(reinterpret_cast<const char *>(&bitSize), 2);                              // サンプルあたりのビット数
+        of.write("data", 4);                                                                // dataヘッダ
+        dataStartPos = of.tellp();                                                          // data開始位置を保存しておく
+        of.seekp(sizeof(char) * 4, std::ios::cur);                                          // 終了までわからないサイズ書き込み用スペースを開ける
     }
 
     void write(char *out, size_t size)
@@ -57,15 +39,11 @@ class RIFF
     // ファイルを閉じる前に、ファイルサイズを保存してお片付け
     ~RIFF()
     {
-        int buf = 0;
-        int last_p = of.tellp();
-        of.seekp(riff_p, std::ios::beg);
-        buf = last_p - 8;
-        of.write((char *)&buf, 4);
-        of.seekp(data_p, std::ios::beg);
-        buf = last_p - data_p - 4;
-        of.write((char *)&buf, 4);
-
+        int fileSize = of.tellp();
+        of.seekp(riffStartPos, std::ios::beg);                                             // チャンクサイズ書き込み位置に移動
+        of.write(reinterpret_cast<const char *>(new int(fileSize - 8)), 4);                // チャンクサイズ 総データ数 - RIFFヘッダ(4byte)とWAVヘッダ(4byte)
+        of.seekp(dataStartPos, std::ios::beg);                                             // サブチャンクサイズ書き込み位置に移動
+        of.write(reinterpret_cast<const char *>(new int(fileSize - dataStartPos - 4)), 4); // サブチャンクサイズ 総データ数 - RIFFヘッダ＋WAVヘッダ + チャンクヘッダ(126byte)
         of.close();
     }
 };
